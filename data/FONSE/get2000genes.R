e503fa3676e99e8b4b25fdc579cf9d8837f366ed
Jeremy Rogers <rogers.jeremyn@gmail.com>
2016-01-27 12:39:19 -0500

    updating data sets

--- data/FONSE/get2000genes.R
+++ data/FONSE/get2000genes.R
@@ -2,12 +2,12 @@ library(seqinr)
 
 idx <- sort(sample(1:5530, 2000, replace=F))
 
-phi <- read.table("yeast.sim.Xobs.csv", sep=",", header=T)
-phi2000 <- phi[idx,]
+phi <- read.table("Scereviciae.phi_estm.csv", sep=",", header=T)
+phi2000 <- phi[idx,c(1,2)]
 colnames(phi2000) <- c("Gene", "phi")
 write.table(phi2000, file = "nse2000.phi.csv", sep=",", row.names=F, col.names=T, quote=F)
 
-genome <- read.fasta(file="yeast.sim.fasta")
+genome <- read.fasta(file="Scereviciae.fasta")
 
 names <- getName(genome)
 
