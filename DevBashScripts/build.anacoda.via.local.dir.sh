7fe02bf9f602b7d6ab22a80754ffea26c0f67716
Michael Gilchrist <mikeg@utk.edu>
2024-03-19 15:29:28 -0400

    update build.anacoda...sh scripts

--- DevBashScripts/build.anacoda.via.local.dir.sh
+++ DevBashScripts/build.anacoda.via.local.dir.sh
@@ -17,22 +17,39 @@
 #
 #  Local install
 
-rVersion=$(Rscript -e 'cat(paste0(R.version$major, ".", sub("\\.[0-9]+", "", R.version$minor)))')
-echo "$rVersion"
-installDir="~/R/lib/$rVersion-dev/"
-echo "$installDir"
+if [[ "$#" -eq 0 ]]; then
+    echo "sourceDir unspecified, using default value: \"./RibModelFramework\"";
+    sourceDir="RibModelFramework";
+else
+    echo "Using sourceDir: $1"
+    sourceDir="$1"
+fi
 
-if R CMD build --no-build-vignettes RibModelFramework ; then
+rVersion=$(Rscript -e 'cat(paste0(R.version$major, ".", sub("\\.[0-9]+", "", R.version$minor)))')
+echo "R Version: $rVersion"
+branch=$(git -C "$sourceDir" branch --show-current)
+echo "Git Branch: $branch"
+#installDir="~/R/lib/$rVersion-dev/"
+installDir=~/R/lib/"$branch"
+# ensure directory exists
+mkdir -p "$installDir"
+echo "Install Dir: $installDir"
+
+
+
+
+if R CMD build --no-build-vignettes "$sourceDir"; then
     echo "Build succeeded. Installing package";
     MAKE="make -j$(($(nproc)-1))"; #use 1 less than number of cores on machine
     if R CMD INSTALL -l "$installDir" $(ls -t AnaCoDa_*.tar.gz| head -1) ; then
 	echo "Install succeeded."
 	## test code
-	cd RibModelFramework/tests;
+	#cd "$sourceDir/tests";
 	## keep this one long command (note the double quotes
 	if Rscript -e ".libPaths(\"$installDir\"); 
-	   .libPaths(); \\
-	   source(\"testthat.R\"); \\
+	   .libPaths();
+           setwd(\"$sourceDir/tests/\");
+	   source(\"./testthat.R\");
 	   packageVersion(\"AnaCoDa\")"; 
 	then
 	    echo "TestThat succeeded"
